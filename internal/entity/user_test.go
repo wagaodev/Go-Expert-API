ae2e385704582eca3a3606a739e4a52ee9666820
Wagner Goulart ( Wagão ) <wagnergoulart@brq.com>
2024-03-31 02:00:47 -0300

    test: test create new user

---
+++ internal/entity/user_test.go
@@ -0,0 +1,17 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewUser(t *testing.T) {
+	user, err := NewUser("John Doe", "j@j.com", "123456")
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.NotEmpty(t, user.ID)
+	assert.NotEmpty(t, user.Password)
+	assert.Equal(t, "John Doe", user.Name)
+	assert.Equal(t, "j@j.com", user.Email)
+}
