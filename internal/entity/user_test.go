6bdbd2cd5def987682d252e06ac24bb9a6022dd8
Wagner Goulart ( Wagão ) <wagnergoulart@brq.com>
2024-03-31 02:08:10 -0300

    test: validate password created

--- internal/entity/user_test.go
+++ internal/entity/user_test.go
@@ -6,12 +6,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNewUser(t *testing.T) {
+func TestUser_NewUser(t *testing.T) {
 	user, err := NewUser("John Doe", "j@j.com", "123456")
 	assert.Nil(t, err)
 	assert.NotNil(t, user)
 	assert.NotEmpty(t, user.ID)
+	assert.NotEmpty(t, user.Email)
 	assert.NotEmpty(t, user.Password)
 	assert.Equal(t, "John Doe", user.Name)
 	assert.Equal(t, "j@j.com", user.Email)
+}
+
+func TestUser_ValidatePassword(t *testing.T) {
+	user, err := NewUser("John Doe", "j@j.com", "123456")
+	assert.Nil(t, err)
+	assert.True(t, user.ValidatePassword("123456"))
+	assert.False(t, user.ValidatePassword("12345t67"))
 }
